27460e10931505b575d75151f251d834ca96e50e
HugoHead <deanmartin@830>
2024-11-10 15:51:10 -0500

    Started claw program base

---
+++ src/main/include/subsystems/ClawHAL.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "SubSystemConfig.h"
+#include <frc/Timer.h>
+#include <frc/trajectory/TrapezoidProfile.h>
+#include <units/length.h>
+#include <units/velocity.h>
+#include <units/angle.h>
+#include <units/acceleration.h>
+#include <frc/Timer.h>
+#include <frc/trajectory/TrapezoidProfile.h>
+
+class ClawHAL
+{
+
+
+
+
+
+};

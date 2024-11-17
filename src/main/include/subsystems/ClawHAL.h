014b5b6b64e9f9801e44216e03b329d6047fd9e9
Student <student@ratpack>
2024-11-17 15:56:06 -0500

    Added changes to ClawHAL

--- src/main/include/subsystems/ClawHAL.h
+++ src/main/include/subsystems/ClawHAL.h
@@ -12,9 +12,35 @@
 
 class ClawHAL
 {
+private:
+ double m_intakeSpeed = 0.0;
 
-
-
-
-
+    public:
+        ClawHAL();
+        ~ClawHAL() = default;
+        void  ManualMovePivot(double speed);
+        double  GetAngle(); 
+        void  ProfiledMoveToAngle(double angle);
+        double  GetSpeed();
+        void  SetPosition();
+        void  PublishDebugInfo();
+        void  ResetProfiledMoveState(); 
 };
+
+/*
+class LauncherHAL
+{
+    public:    
+        LauncherHAL();
+        ~LauncherHAL() = default;
+        void SetClawHALSpeed(double speed);
+        double GetClawHALSpeed();
+        void RunIntake(double speed);
+        void ResetProfiledMoveState();
+
+
+    private:
+        double m_indexerSpeed;
+        int m_profileState = 0;
+};
+*/

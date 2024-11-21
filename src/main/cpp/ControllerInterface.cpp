8b630bf6f0e4ab8289eed3b21ebbb11714c332a8
Harsh-Sinha <harsh.sinha@hotmail.com>
2024-11-21 16:59:57 -0500

    get rid of references to old stuff in cpp files

--- src/main/cpp/ControllerInterface.cpp
+++ src/main/cpp/ControllerInterface.cpp
@@ -2,38 +2,7 @@
 
 void ControllerInterface::UpdateRobotControlData(RobotControlData &controlData)
 {
-    UpdateIntakeInput(controlData);
-    UpdateLauncherInput(controlData);
     UpdateSwerveInput(controlData);
-    UpdateVisionInput(controlData);
-};
-
-void ControllerInterface::UpdateIntakeInput(RobotControlData &controlData)
-{
-    controlData.intakeInput.goToAmpPos = m_copilot.GetLeftY() > 0.5;
-    controlData.intakeInput.goToStowPos = m_copilot.GetLeftY() < -0.5;
-    controlData.intakeInput.goToPseudoStowPos = m_copilot.GetLeftX() > 0.5;
-    controlData.intakeInput.runIntakeOutSlow = m_copilot.GetLeftTriggerAxis() >= 0.2;
-
-    if (controlData.intakeOutput.intakePos == IntakePos::AMP)
-    {
-        controlData.intakeInput.runIntakeIn = m_copilot.GetRightTriggerAxis() > 0.2;
-        controlData.launcherInput.runIndexerForward = false;
-    }
-    else
-    {
-        controlData.launcherInput.runIndexerForward = m_copilot.GetRightTriggerAxis() > 0.2;
-        controlData.intakeInput.runIntakeIn = false;
-    }
-
-};
-
-void ControllerInterface::UpdateLauncherInput(RobotControlData &controlData)
-{
-    controlData.launcherInput.goToStowPos = m_copilot.GetAButton();
-    controlData.launcherInput.goToSubPos = m_copilot.GetYButton();
-    controlData.launcherInput.runIndexerBackward = m_copilot.GetLeftTriggerAxis() >= 0.2;
-    controlData.launcherInput.goToHoardePos = m_copilot.GetBButton();
 };
 
 void ControllerInterface::UpdateSwerveInput(RobotControlData &controlData)
@@ -60,7 +29,3 @@ void ControllerInterface::UpdateSwerveInput(RobotControlData &controlData)
         controlData.swerveInput.resetGyroZeroHeading = false;
     }
 };
-
-void ControllerInterface::UpdateVisionInput(RobotControlData &controlData) {
-    // FIXME 
-}

ab59853ebe7445fb2f8855b2c2dde0fc7bf49139
Student <student@ratpack>
2024-12-01 14:24:04 -0500

    added more to elevator

--- src/main/cpp/HAL/PlaceBox.cpp
+++ src/main/cpp/HAL/PlaceBox.cpp
@@ -1,48 +1,33 @@
-#include <frc/Timer.h>
-#include "subsystems/PlaceBoxHAL.h"
+#include "HAL/PlaceBoxHAL.h"
 #include <iostream>
 #include <ctime>
 #include <networktables/DoubleTopic.h>
 #include <networktables/IntegerTopic.h>
 #include <networktables/StringTopic.h>
-frc::Timer times;
 double levelHeight;
 int level;
-double orignalRaiseSpeed;
 double robotHeight;
-double changeInAcceleration;
 double baseHeight;
-double weight;
 double shelfPosition;
 double boxSide;
 double shelfWidth;
 double position=0;
 double boxPosition=0;
 double distanceFromThePlatform=0;
-double raiseSpeed=0;
 double shelfWidth=0;
-double heightFromPlatform=0;
 double distanceFromPlatform(double position, double shelfPosition)
 {
     double distance = shelfPosition-position;
     return distance;
 }
 double distanceFromThePlatform = distanceFromPlatform(position, shelfPosition);
-double heightFromPlatform(int level, double levelHeight, double baseHeight){
-    double heightDifference = level*levelHeight - baseHeight;
-    return heightDifference;
-}
-double raisingSpeed(double originalRaiseSpeed, double weight, double changeInAcceleration){
-    double actualRaiseSpeed = originalRaiseSpeed*changeInAcceleration;
-    return actualRaiseSpeed;
-}
-double raiseSpeed = raisingSpeed(orignalRaiseSpeed, weight, changeInAcceleration);
-double heightFromPlatform = heightFromPlatform(level, levelHeight, baseHeight);
 
-void liftBox(double heightDistance, double speed){
-    times.Start();
-    while((times.Get() > units::millisecond_t(1000*heightDistance/speed))){
-
+int getLevel(){
+    return int(robotHeight/levelHeight)-1;
+}
+void liftBox(int level){
+    while(getLevel()<level){
+        //lift
     }
 }
 void moveToShelf(double shelfPosition, double position, double distanceFromPlatform, double boxSides){
@@ -53,20 +38,15 @@ void slideBoxIntoShelf(double shelfWidth, double boxPosition, double shelfPositi
     while(shelfPosition+shelfWidth > boxPosition+boxSides+1){
     }
 }
-void putBox(double distance, double heightDistance, double speed, double boxSides, double shelfPosition, double boxPosition){
-    liftBox(heightDistance, speed);
+void putBox(double distance, int goalLevel, double boxSides, double shelfPosition, double boxPosition){
+    liftBox(goalLevel);
     moveToShelf(shelfPosition, position, distanceFromThePlatform, boxSides);
     slideBoxIntoShelf(shelfWidth, boxPosition, shelfPosition, boxSides);
 
 double levelHeight;
-double orignalRaiseSpeed;
 double robotHeight;
-double changeInAcceleration;
 double baseHeight;
-double weight;
 double shelfPosition;
 double boxSide;
 double shelfWidth;
-frc::Timer m_timer;
 }
-
--- src/main/include/HAL/PlaceBoxHAL.h
+++ src/main/include/HAL/PlaceBoxHAL.h
@@ -9,21 +9,14 @@ class PlaceBoxs{
         double position;
         double boxPosition;
         double distanceFromThePlatform;
-        double raiseSpeed;
         double shelfWidth;
-        double heightFromPlatform;
         double distanceFromPlatform(double position);
-        double heightFromPlatform(int level, double levelHeight, double baseHeight);
-        int raiseSpeed(double noriginalRaiseSpeed, double weight, double changeInAcceleration);
-        void putBox(double distance, double heightDistance, double speed, double boxSides, double shelfPosition, double boxPosition);
+        void putBox(double distance, int goallevel, double boxSides, double shelfPosition, double boxPosition);
         
     private:
         double levelHeight;
-        double orignalRaiseSpeed;
         double robotHeight;
-        double changeInAcceleration;
         double baseHeight;
-        double weight;
         double shelfPosition;
         double boxSide;
         double shelfWidth;
